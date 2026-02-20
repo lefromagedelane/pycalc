d37a1ea8c737f3559ba429da86ae543f4ba96024
Romain Hoogstoel <22501809@me.uvsq.fr>
2026-02-20 11:26:26 +0100

    division

--- pycalc.py
+++ pycalc.py
@@ -3,9 +3,9 @@ def calculatrice():
     val1 = int(input("Ecrire le premier nombre : "))
     operateur = input("Ecrire l'opérateur souhaité : ")
     val2 = int(input("Ecrire le deuxième nombre : "))
-
-    if operateur == "-":
-        resultat = val1 - val2
+    
+    if operateur == "/":
+        resultat = val1 / val2
         print("Résultat :", resultat)
 
 
