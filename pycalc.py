9242c636fce7fa42aad14d079f23e67a0af38c21
Romain Hoogstoel <22501809@me.uvsq.fr>
2026-02-20 11:14:58 +0100

    programme complet

--- pycalc.py
+++ pycalc.py
@@ -1,2 +1,23 @@
 def calculatrice():
-    pass
+    
+    val1 = int(input("Ecrire le premier nombre : "))
+    operateur = input("Ecrire l'opérateur souhaité : ")
+    val2 = int(input("Ecrire le deuxième nombre : "))
+    
+    if operateur == "+":
+        resultat = val1 + val2
+        print("Résultat :", resultat)
+
+    if operateur == "-":
+        resultat = val1 - val2
+        print("Résultat :", resultat)
+
+    if operateur == "/":
+        resultat = val1 / val2
+        print("Résultat :", resultat)
+
+    if operateur == "*":
+        resultat = val1 * val2
+        print("Résultat :", resultat)
+
+calculatrice()
