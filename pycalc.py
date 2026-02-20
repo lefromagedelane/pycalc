e77f5f8ca0ae3fbe122b8bffe7c2ea99013d67cb
Romain Hoogstoel <22501809@me.uvsq.fr>
2026-02-20 11:25:55 +0100

    soustraction

--- pycalc.py
+++ pycalc.py
@@ -3,11 +3,10 @@ def calculatrice():
     val1 = int(input("Ecrire le premier nombre : "))
     operateur = input("Ecrire l'opérateur souhaité : ")
     val2 = int(input("Ecrire le deuxième nombre : "))
-    
-    if operateur == "+":
-        resultat = val1 + val2
+
+    if operateur == "-":
+        resultat = val1 - val2
         print("Résultat :", resultat)
 
-    
 
 calculatrice()
